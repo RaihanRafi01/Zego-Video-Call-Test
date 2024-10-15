d4615d7843165220bbe291578092ac7efffc273d
Raihan <raihanrafi992@gmail.com>
2024-10-15 16:59:31 +0600

    PIP added

--- android/app/src/main/kotlin/com/example/videocalling_test/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/videocalling_test/MainActivity.kt
@@ -1,5 +1,23 @@
 package com.example.videocalling_test
 
+import android.app.PictureInPictureParams
+import android.os.Build
+import android.util.Rational
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun onUserLeaveHint() {
+        super.onUserLeaveHint()
+
+        // Enter Picture-in-Picture (PiP) mode when the user presses the home button or minimizes the app.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val aspectRatio = Rational(16, 9)
+            val params = PictureInPictureParams.Builder()
+                .setAspectRatio(aspectRatio)
+                .build()
+            enterPictureInPictureMode(params)
+        }
+    }
+}
+
+
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,47 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Zego Call App'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const CallScreen(),
-              ),
-            );
-          },
-          child: const Text('Start a Call'),
-        ),
-      ),
-    );
-  }
-}
-
 class CallScreen extends StatelessWidget {
-  const CallScreen({Key? key}) : super(key: key);
-
+  const CallScreen({super.key});
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Voice/Video Call'),
-      ),
       body: ZegoUIKitPrebuiltCall(
-        appID: 4718390, // Replace with your Zego appID
-        appSign: 'c16cd1e0bc62f20a10c43ecac646aaafd7f352af5a01687b235fd0d03aece263', // Replace with your Zego appSign
-        userID: 'user_1', // User ID of the current user
-        userName: 'User 1', // Name of the current user
-        callID: 'call_1', // Unique call ID
+        appID: 4718390,
+        appSign: 'c16cd1e0bc62f20a10c43ecac646aaafd7f352af5a01687b235fd0d03aece263',
+        userID: 'user_2',
+        userName: 'User 2',
+        callID: 'call_1',
         config: ZegoUIKitPrebuiltCallConfig.oneOnOneVideoCall(),
       ),
     );
